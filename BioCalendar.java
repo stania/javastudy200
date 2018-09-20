79f8381d755cd2f4970b1f26ba71f66fe1c4bf3e
Moon Yon Kim <moonyonkim@eediom.com>
2018-09-20 18:19:41 +0900

    Create BioCalendar.java
    added java study chapter028 BioCalendar.java file.

---
+++ BioCalendar.java
@@ -0,0 +1,16 @@
+package kr.co.infopub.chapter.s028;
+
+// Math.sin(), s014.Math.toDegrees()
+
+
+public class BioCalendar {
+
+	// 상수, 상수값은 변경할 수 없다.
+	public static final int PHYSICAL = 23;     // 상수(개발자 정의)
+	public static void main(String[] args) {
+		int index=PHYSICAL;                  // 상수값을 변수에 대입
+		int days=1200;     
+		double phyval=100*Math.sin( (days % index) * 2 * Math.PI / index );   // TODO: phyval 구하는 함수 구현 
+		System.out.printf("나의 신체지수 %1$.2f입니다.\n",phyval);
+	}
+}
